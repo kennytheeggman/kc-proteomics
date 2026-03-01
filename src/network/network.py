ce897c6619b2de7cf411d4daea2889d1edad0640
kennytheeggman <kenny.kh.huang@gmail.com>
2026-03-01 00:15:07 -0500

    created the rest of the network

--- src/network/network.py
+++ src/network/network.py
@@ -1,1 +1,25 @@
 # composition of modules into a) spectrum to latent to sequence and b) latent to spectrum
+
+
+from torch import nn
+
+from src.network.latent import EmbedDecode, EmbedEncode
+from src.network.sequence import EmbedSequence
+from src.network.spectrum import SpecEmbed
+from src.utils.config import Config
+
+
+class Model(nn.Module):
+    def __init__(self, config: Config):
+        super().__init__()
+        self.se = SpecEmbed(config)
+        self.ee = EmbedEncode(config)
+        self.es = EmbedSequence(config)
+        self.ed = EmbedDecode(config)
+
+    def forward(self, mz, i):
+        embed = self.se.forward(mz, i)
+        encoded = self.ee.forward(embed, embed[0])
+        prob_matrix = self.es.forward(encoded)
+        decoded = self.ed.forward(encoded)
+        return prob_matrix, decoded
