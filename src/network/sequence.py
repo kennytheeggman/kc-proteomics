bf2153ecea9a5a815bb821ada6b0c073cfb9208b
kennytheeggman <kenny.kh.huang@gmail.com>
2026-03-01 00:06:44 -0500

    add sequencing transformer section

--- src/network/sequence.py
+++ src/network/sequence.py
@@ -1,1 +1,36 @@
 # latent space to sequence and sequence to latent space (optional) modules
+
+
+from math import ceil
+from torch import nn
+import torch
+from src.utils.config import Config
+
+
+class EmbedSequence(nn.Module):
+    def __init__(self, config: Config):
+        super().__init__()
+        self.d_model = config.embed.d_model
+        self.d_output = config.AA_TYPES
+        self.num_peaks = config.num_peaks
+        self.linear_layers = config.seq.linear_num_layers
+        self.encoder_layers = config.seq.encoder_num_layers
+
+        self.embedding_stack = nn.TransformerEncoder(
+            nn.TransformerEncoderLayer(config.embed.d_model, self.num_peaks),
+            self.encoder_layers
+        )
+
+        linear_layers = [
+            nn.Linear(
+                ceil((self.d_output - self.d_model) * (i / self.linear_layers) + self.d_model), 
+                ceil((self.d_output - self.d_model) * ((i + 1) / self.linear_layers) + self.d_model)
+            ) for i in range(self.linear_layers)
+        ]
+        self.vector_stack = nn.Sequential(*linear_layers)
+
+    def forward(self, embedding):
+        raw_values = self.embedding_stack(embedding)
+        raw_matrix = self.vector_stack(raw_values)
+        prob_matrix = torch.softmax(raw_matrix, dim=0)
+        return prob_matrix
