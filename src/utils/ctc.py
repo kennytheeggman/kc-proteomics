9a83d1912714a2e6c967d4e6de0b73e77fb9f274
kennytheeggman <kenny.kh.huang@gmail.com>
2026-03-06 20:59:03 -0500

    modified learning rate will need to do grad clipping or wtv it's called

--- src/utils/ctc.py
+++ src/utils/ctc.py
@@ -2,8 +2,8 @@ import torch
 
 
 def decode(prob_matrix, log=False):
-    if log:
-        return torch.argmin(prob_matrix, dim=1)
+    # if log:
+    #     return torch.argmin(prob_matrix, dim=1)
     return torch.argmax(prob_matrix, dim=1)
 
 def reduce(sequence: torch.Tensor, blank=0):
