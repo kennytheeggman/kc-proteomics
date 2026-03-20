86e6ffbfe79539822898b9e3cb3fec86ccdebf96
kennytheeggman <kenny.kh.huang@gmail.com>
2026-03-20 17:32:15 -0400

    use cross entropy loss on the resulting tokens

--- src/utils/loss.py
+++ src/utils/loss.py
@@ -1,29 +1,25 @@
-
-
 import torch
+import torch.nn as nn
 
 from .ctc import encode
 
+
 def get_loss(config):
-    ctc_loss = torch.nn.CTCLoss(blank=0, reduction="sum", zero_infinity=True)
-    mse_loss = torch.nn.MSELoss()
+    ce_loss = nn.CrossEntropyLoss(
+        ignore_index=config.PAD,
+        label_smoothing=config.hyper.label_smoothing
+    )
+    mse_loss = nn.MSELoss()
 
-    def loss(prob_matrix: torch.Tensor, embed: torch.Tensor, decoded: torch.Tensor, sequences):
+    def loss(logits: torch.Tensor, embed: torch.Tensor, decoded: torch.Tensor, tgt: torch.Tensor, tgt_padding_mask: torch.Tensor):
+        batch_size, seq_len, vocab_size = logits.shape
+        
+        logits_flat = logits.view(-1, vocab_size)
+        tgt_flat = tgt.view(-1)
+        
+        loss_ce = ce_loss(logits_flat, tgt_flat)
+        loss_mse = mse_loss(embed, decoded)
 
-        # ctc loss is NOT batch first
-        prob_matrix_ctc = prob_matrix.permute(1, 0, 2)
-
-        batch_sz = prob_matrix_ctc.shape[1]
-        seq_len = prob_matrix_ctc.shape[0]
-
-        # set up things for ctc
-        encoded_sequences = torch.cat([encode(s.decode()) for s in sequences])  # flat 1d tensor
-        input_lengths = torch.full((batch_sz,), seq_len, dtype=torch.long)  # lengths of each spectrum seq (where to slice encoded_sequences)
-        target_lengths = torch.tensor([len(s.decode()) for s in sequences], dtype=torch.long)  # lengths of each peptide seq (where to slice encoded peptide sequences)
-
-        loss1 = ctc_loss(prob_matrix_ctc, encoded_sequences, input_lengths, target_lengths)
-        loss2 = mse_loss(embed, decoded)
-
-        return loss1 * config.hyper.ctc_weight + loss2 * config.hyper.mse_weight
+        return loss_ce # + loss_mse * config.hyper.mse_weight
 
     return loss
