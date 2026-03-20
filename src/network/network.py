0f29ed77a932c5da2edf58d7ee45d5b53e2a5b67
kennytheeggman <kenny.kh.huang@gmail.com>
2026-03-20 17:34:37 -0400

    update network composition

--- src/network/network.py
+++ src/network/network.py
@@ -1,6 +1,3 @@
-# composition of modules into a) spectrum to latent to sequence and b) latent to spectrum
-
-
 from torch import nn
 
 from ..network.latent import EmbedDecode, EmbedEncode
@@ -12,14 +9,31 @@ from ..utils.config import Config
 class Model(nn.Module):
     def __init__(self, config: Config):
         super().__init__()
+        self.config = config
         self.se = SpecEmbed(config)
         self.ee = EmbedEncode(config)
         self.es = EmbedSequence(config)
         self.ed = EmbedDecode(config)
 
-    def forward(self, mz, i, premz):
+    def forward(self, mz, i, premz, tgt=None, tgt_key_padding_mask=None):
         seq_embed, p_embed = self.se.forward(mz, i, premz)
-        encoded = self.ee.forward(seq_embed, p_embed)  # seq_embed shape is (seq_len, d_model) and p_embed shape is (d_model)
-        prob_matrix = self.es.forward(encoded)
+        encoded = self.ee.forward(seq_embed, p_embed)
+        
+        if tgt is not None:
+            logits = self.es.forward(encoded, tgt, tgt_key_padding_mask)
+            decoded = self.ed.forward(encoded)
+            return logits, seq_embed, decoded
+        
+        generated = self.es.forward(encoded)
         decoded = self.ed.forward(encoded)
-        return prob_matrix, seq_embed, decoded
+        return generated, seq_embed, decoded
+
+    def encode(self, mz, i, premz):
+        seq_embed, p_embed = self.se.forward(mz, i, premz)
+        encoded = self.ee.forward(seq_embed, p_embed)
+        return encoded
+
+    def decode_sequence(self, memory, tgt=None, tgt_key_padding_mask=None):
+        if tgt is not None:
+            return self.es.forward(memory, tgt, tgt_key_padding_mask)
+        return self.es.forward(memory)
